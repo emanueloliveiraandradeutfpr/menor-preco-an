2fd8904aae11d883949133bebb627dac735570d4
emanueloliveiraandrade <emanueloliveiraandrade@alunos.utfpr.edu.br>
2024-09-03 18:36:37 -0300

    feat: navigation added to the header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { MyListComponent } from './components/my-list/my-list.component';
+import { AccountComponent } from './components/account/account.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'list',
+    component: MyListComponent,
+  },
+  {
+    path: 'account',
+    component: AccountComponent,
+  },
+];
---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,11 @@
+import { NgOptimizedImage } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [NgOptimizedImage],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent {}
