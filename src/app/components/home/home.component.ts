f5de648319c2f9293d6a9eb5bf29e09279c460fc
emanueloliveiraandrade <emanueloliveiraandrade@alunos.utfpr.edu.br>
2024-09-03 19:44:31 -0300

    feat: add footer

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import { ScannerComponent } from '../scanner/scanner.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, ScannerComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
