5ccc0dc4b759658a673f8ba0e09ec9dcd1cc7941
emanueloliveiraandrade <emanueloliveiraandrade@alunos.utfpr.edu.br>
2024-09-06 22:37:07 -0300

    feat: create form structure

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from '../home/home.component';
 import { NgOptimizedImage } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [HomeComponent, NgOptimizedImage],
+  imports: [HomeComponent, NgOptimizedImage, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,34 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 import { ScannerComponent } from '../scanner/scanner.component';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NgOptimizedImage, ScannerComponent],
+  imports: [NgOptimizedImage, ScannerComponent, ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  useForm: FormGroup;
+
+  constructor() {
+    this.useForm = new FormGroup({
+      searchTerm: new FormControl('', [
+        Validators.minLength(2),
+        Validators.required,
+      ]),
+    });
+  }
+
+  onSubmit() {
+    console.log(this.useForm.value);
+    this.useForm.reset();
+  }
+}
